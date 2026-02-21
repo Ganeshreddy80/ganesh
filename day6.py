e5f88d2785ae89f7104bb18de01259a358083fc3
A.Ganesh Kumar Reddy <ganeshkumarreddyakula@gmail.com>
2026-02-21 23:23:24 +0530

    Implement Smart Playlist Intelligence System

---
+++ day6.py
@@ -0,0 +1,43 @@
+#Smart Playlist Intelligence System
+a=int(input("Enter Number of songs: "))
+songs=[0]*a
+for i in range(a):
+    songs[i]=int(input("Enter duration: "))
+
+invalid=False
+for j in songs:
+    if j<=0:
+        invalid=True
+        break
+        
+if invalid:
+    print("Invalid Playlist")
+else:
+    Total_Duration=sum(songs)
+    cnt_song=len(songs)
+
+    repeat=False
+    for j in songs:
+        if songs.count(s)>1:
+            repeat=True
+            break
+    if Total_Duration<300:
+        c="Too Short Playlist"
+        r="Add more songs"
+    elif Total_Duration>3600:
+        c="Too Long Playlist"
+        r="Remove Some Songs"
+    elif repeat:
+        c="Repetitive Playlist"
+        r="Add Vareity"
+    elif max(songs)-min(songs)<=300:
+        c="Balanced Playlist"
+        r="Good Listening session"
+    else:
+        c="Irregular Playlist"
+        r="Make sure that duration vary reasonably"
+    
+    print("Total Duration: ",Total_Duration,"seconds")
+    print("songs: ",cnt_song)
+    print("Category: ",c)
+    print("Recommendation: ",r)
